137beec24ff193488cbe7943ec11248fff4ce7ea
GorskiMarekGM <gorskimarekgm@gmail.com>
2023-05-19 16:14:31 +0200

    del cache

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
-# str 80 ksiazki
+# Setup allows for distribution of the todo package
+# It shows python that name is 'todo' and should install todo package
 setup(name='todo',packages=['todo'])
 
